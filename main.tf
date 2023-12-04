10f4ebc0e07f0c9b79e6e4a18bf883c89f587fd8
scent-y <chewbacca200_walkingc@yahoo.co.jp>
2023-12-04 14:44:34 +0900

    add resource

--- main.tf
+++ main.tf
@@ -10,9 +10,28 @@ terraform {
 provider "google" {
     credentials = file("./sa.json")
     project = "{project_id}"
-    region = "asia-northeast1-a"
+    region = "asia-northeast1"
+    zone = "asia-northeast1-a"
 }
 
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+    name = "terraform-instance"
+    machine_type = "f1-micro"
+
+    boot_disk {
+      initialize_params {
+        image = "debian-cloud/debian-11"
+      }
+    }
+
+    network_interface {
+      network = google_compute_network.vpc_network.name // これにより2つのリソース間の依存関係が確立される
+      access_config {
+      }
+    }
+  
 }
