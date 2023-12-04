dbe8213eeb57e9d4be37481f175f550eaf28e6ae
scent-y <chewbacca200_walkingc@yahoo.co.jp>
2023-12-04 16:25:42 +0900

    terraform fmt

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 terraform {
-    required_providers {
-        google = {
-            source = "hashicorp/google"
-            version = "4.51.0"
-        }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
     }
+  }
 }
 
 provider "google" {
-    credentials = file(var.credentials_file)
-    project = var.project
-    region = var.region
-    zone = var.zone
+  credentials = file(var.credentials_file)
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
 }
 
 resource "google_compute_network" "vpc_network" {
@@ -19,20 +19,20 @@ resource "google_compute_network" "vpc_network" {
 }
 
 resource "google_compute_instance" "vm_instance" {
-    name = "terraform-instance"
-    machine_type = "f1-micro"
-    tags = ["web","dev"] // ネットワークタグ
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"] // ネットワークタグ
 
-    boot_disk {
-      initialize_params {
-        image = "cos-cloud/cos-stable"
-      }
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
     }
+  }
 
-    network_interface {
-      network = google_compute_network.vpc_network.name // これにより2つのリソース間の依存関係が確立される
-      access_config {
-      }
+  network_interface {
+    network = google_compute_network.vpc_network.name // これにより2つのリソース間の依存関係が確立される
+    access_config {
     }
-  
+  }
+
 }
